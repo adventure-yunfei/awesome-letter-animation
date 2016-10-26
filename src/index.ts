8cf28acb8ad0e1ac4c55e681eb589d068f835087
adventure-yunfei <adventure030@gmail.com>
2016-10-26 16:56:16 +0800

    export func

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 const d3: any = require('d3');
 import {buildAwesomeLetterAnimation} from './letter/letter';
 
-const svg = d3
-    .select('body')
-    .style('background-color', 'black')
-    .append('svg')
-    .attr('width', 1800)
-    .attr('height', 800);
 
-buildAwesomeLetterAnimation('i am here', svg, {x: 200, y: 200});
-// buildAwesomeLetterAnimation('中   二', svg, {x: 200, y: 200});
+function _testHello() {
+    const svg = d3
+        .select('body')
+        .style('background-color', 'black')
+        .append('svg')
+        .attr('width', 1800)
+        .attr('height', 800);
+
+    buildAwesomeLetterAnimation('i am here', svg, {x: 200, y: 200});
+    // buildAwesomeLetterAnimation('中   二', svg, {x: 200, y: 200});
+}
+
+window['awesomeLetterAnimation'] = {
+    buildAwesomeLetterAnimation,
+    _testHello
+};
+
+export {
+    buildAwesomeLetterAnimation
+}
