302636144a029d342d5e736c23d24c6103a4f0d0
adventure-yunfei <adventure030@gmail.com>
2016-10-27 17:38:59 +0800

    export d3 & animejs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 declare var require: any;
 declare var module: any;
 const d3: any = require('d3');
+const animejs: any = require('animejs');
 import {buildAwesomeLetterAnimation} from './letter/letter';
 
 function awesomeLetterAnimation(str: string) {
@@ -24,5 +25,8 @@ awesomeLetterAnimation['_testHello'] = function () {
     buildAwesomeLetterAnimation('hello world', svg, {x: 200, y: 200});
     // buildAwesomeLetterAnimation('中   二', svg, {x: 200, y: 200});
 };
+
+awesomeLetterAnimation['d3'] = d3;
+awesomeLetterAnimation['animejs'] = animejs;
 
 module.exports = awesomeLetterAnimation;
